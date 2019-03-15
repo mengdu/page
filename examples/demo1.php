9d750d05a2db4597c4f6ef1499569d389c1cea6c
v_yguiliu <v_yguiliu@tencent.com>
2019-03-15 15:26:20 +0800

    del: 移除旧代码

--- examples/demo1.php
+++
@@ -1,8 +0,0 @@
-<link href="../css/pagination.css" rel="stylesheet">
-<?php
-include_once('../src/pagination.class.php');
-
-$page = new Pagination(100, 9);
-
-print_r($page);
-print_r($page->links());
